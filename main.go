3b32c395597d5a8b76a0ca15613424c4907bd4dd
Soenke J. Peters <peters__git@opcenter.de>
2018-09-20 20:19:45 +0200

    start working on xml info

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"strconv"
 	"time"
 
@@ -17,10 +19,27 @@ func init() {
 
 var getSysDeviceIdent optolink.FsmCmd = optolink.FsmCmd{ID: [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}, Command: 0x01, Address: [2]byte{0x00, 0xf8}, Args: nil, ResultLen: 8}
 
+// const testDeviceIdent = [8]byte{0x20, 0x92, 0x01, 0x07, 0x00, 0x00, 0x01, 0x5a}
+
+var inputFile = flag.String("i", "ecnDataPointType.xml", "filename of ecnDataPointType.xml like file")
+
 func main() {
+	flag.Parse()
 	addressHost := "orangepipc"
 	addressPort := 3002
 	address := addressHost + ":" + strconv.Itoa(addressPort)
+
+	xmlFile, err := os.Open(*inputFile)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer xmlFile.Close()
+
+	dpt := &optolink.DataPointType{}
+	var etl optolink.EventTypeList
+	etl = make(optolink.EventTypeList)
+	dpt.EventTypes = etl
 
 	conn := &optolink.Device{}
 	conn.Connect("socket://" + address)
@@ -28,10 +47,20 @@ func main() {
 	cmdChan := make(chan optolink.FsmCmd)
 	resChan := make(chan optolink.FsmResult)
 	go optolink.VitoFsm(conn, cmdChan, resChan)
+
 	cmdChan <- getSysDeviceIdent
 	result, _ := <-resChan
 	fmt.Printf("%# x, %#v\n", result.Body, result.Err)
-     <-time.After(4 * time.Second)
+
+	var id [8]byte
+	copy(id[:], result.Body[:8])
+	err = optolink.FindDataPointType(xmlFile, id, dpt)
+	if err != nil {
+		return
+	}
+	fmt.Printf("%#v\n", dpt)
+
+	<-time.After(4 * time.Second)
 
 	fmt.Println("Nö!")
 
---
+++ optolink/xmlinfo.go
@@ -0,0 +1,153 @@
+package optolink
+
+import (
+	"encoding/xml"
+	"errors"
+	"io"
+	"strconv"
+	"strings"
+)
+
+// xDataPointType is a type to hold raw information read from xml
+type xDataPointType struct {
+	ID                          string `xml:"ID"`
+	EventtTypeList              string `xml:"EventTypeList"`
+	Description                 string `xml:"Description"`
+	Identification              string `xml:"Identification"`
+	IdentificationExtension     string `xml:"IdentificationExtension"`
+	IdentificationExtensionTill string `xml:"IdentificationExtensionTill"`
+}
+
+// DataPointType is a type to describe a DataPoint (aka a Vito* device)
+type DataPointType struct {
+	ID             string
+	Description    string
+	SysDeviceIdent [8]byte
+	EventTypes     EventTypeList
+}
+
+// EventType holds low-level info for commands like address, data format and conversion hints
+type EventType struct {
+	ID      string
+	Address string
+
+	AccessMode string
+	FCRead     string
+	FCWrite    string
+
+	Parameter   string
+	SDKDataType string
+
+	BlockLength  string
+	BytePosition string
+	ByteLength   string
+	BitPosition  string
+	BitLength    string
+
+	Conversion string
+}
+
+// EventTypeList is just a map of EventTyp (aka command) elements
+type EventTypeList map[string]EventType
+
+// EventTypeAliasList may hold aliases or translated names for commands
+type EventTypeAliasList map[string]*EventType
+
+// ErrNotFound is returned when data could not be found
+var ErrNotFound = errors.New("Not found")
+
+// FindDataPointType reads DataPoint info from xml in a format similar to VitoSofts ecnDataPointType.xml format
+func FindDataPointType(xmlReader io.Reader, sysDeviceIdent [8]byte, dpt *DataPointType) error {
+	decoder := xml.NewDecoder(xmlReader)
+	var dp xDataPointType
+
+	for {
+		t, _ := decoder.Token()
+		if t == nil {
+			break
+		}
+		switch se := t.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "DataPointType" {
+				var d xDataPointType
+				decoder.DecodeElement(&d, &se)
+
+				if len(d.Identification) != 4 {
+					break
+				}
+
+				if (len(d.IdentificationExtension) == 0 || (len(d.IdentificationExtension) >= 4 && len(d.IdentificationExtension) <= 6)) &&
+					(len(d.IdentificationExtensionTill) == 0 || (len(d.IdentificationExtensionTill) >= 4 && len(d.IdentificationExtensionTill) <= 6)) {
+
+					i, err := strconv.ParseUint("0x00"+d.Identification, 0, 16)
+					if err != nil {
+						break
+					}
+
+					// Match sysDeviceGroupIdent
+					if sysDeviceIdent[0] != byte((i>>8)&0xff) {
+						break
+					}
+					// Match sysDeviceIdent
+					if sysDeviceIdent[1] != byte(i&0xff) {
+						break
+					}
+
+					idExt, err := strconv.ParseUint("0x00"+d.IdentificationExtension, 0, 24)
+					if err != nil {
+						idExt = 0
+
+					}
+					idExtTill, err := strconv.ParseUint("0x00"+d.IdentificationExtensionTill, 0, 24)
+					if err != nil {
+						idExtTill = 0
+					}
+
+					var dataPointIdExt uint64
+					dataPointIdExt = uint64(sysDeviceIdent[2])<<8 | uint64(sysDeviceIdent[3])
+					if (len(d.IdentificationExtension) > 4) || (len(d.IdentificationExtensionTill) > 4) {
+						dataPointIdExt = uint64(dataPointIdExt)<<16 | uint64(sysDeviceIdent[4])<<8 | uint64(sysDeviceIdent[5])
+					}
+					if dataPointIdExt >= idExt && (dataPointIdExt < idExtTill || idExtTill == 0) {
+						if dp.ID == "" {
+							// First match, break as there is nothing to compare
+							dp = d
+							break
+						}
+
+						dpidExt, err := strconv.ParseUint("0x00"+dp.IdentificationExtension, 0, 24)
+						if err != nil {
+							dpidExt = 0
+						}
+						dpidExtTill, err := strconv.ParseUint("0x00"+dp.IdentificationExtensionTill, 0, 24)
+						if err != nil {
+							dpidExtTill = 0
+						}
+
+						if idExt >= dpidExt && (idExtTill < dpidExtTill || dpidExtTill == 0) {
+							// A better match than the previously found one
+							dp = d
+						}
+					}
+				}
+			}
+		default:
+			//
+		}
+	}
+	if dp.ID != "" {
+		r := dpt
+		r.ID = dp.ID
+		r.Description = dp.Description
+		r.SysDeviceIdent = sysDeviceIdent
+		etl := dpt.EventTypes
+
+		for _, et := range strings.Split(dp.EventtTypeList, ";") {
+			etl[et] = EventType{ID: et}
+		}
+		r.EventTypes = etl
+
+		return nil
+	}
+	return ErrNotFound
+}
