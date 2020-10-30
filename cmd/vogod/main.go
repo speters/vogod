dface672bafb2cceda76446b288538dc93fa4777
Soenke J. Peters <peters__git@opcenter.de>
2020-10-30 16:45:41 +0100

    consistency

--- cmd/vogod/main.go
+++ cmd/vogod/main.go
@@ -34,7 +34,7 @@ var conn *vogo.Device
 var buildVersion = "unspecified"
 var buildDate = "unknown"
 
-const selfDesc = "Vi*ssmann (r) optolink go daemon"
+const selfDesc = "V'mann optolink go daemon"
 
 var getSysDeviceIdent vogo.FsmCmd = vogo.FsmCmd{ID: [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}, Command: 0x01, Address: [2]byte{0x00, 0xf8}, Args: nil, ResultLen: 8}
 
